16ac12ebc0c86ce71772dd75567e1e7747bf1901
Allan L. R. Hansen <allan.lrh@gmail.com>
2014-12-24 15:31:00 +0100

    Initial commit.

---
+++ firefox_user.js
@@ -0,0 +1,4 @@
+user_pref("capability.policy.policynames", "nofocus");
+user_pref("capability.policy.nofocus.sites", "http://www.");
+user_pref("capability.policy.nofocus.sites", "https://www.");
+user_pref("capability.policy.nofocus.HTMLInputElement.focus", "noAccess");
